3b84295365bffcd91c0b700c069450a0744adbee
Lucas Smith <lucas@lucasesmith.com>
2023-05-22 20:45:47 -0400

    Add retry for file downloads.

--- util/file.py
+++ util/file.py
@@ -2,7 +2,8 @@ import os
 
 def file_can_be_downloaded (remote_file_path):
     filename = remote_file_path.split("/")[len(remote_file_path.split()) - 2]
-    command = "wget " + remote_file_path + " -O /tmp/" + filename + " >/dev/null 2>&1 && rm /tmp/" + filename
+    params = "--retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 5"
+    command = "wget " + params + " " + remote_file_path + " -O /tmp/" + filename + " >/dev/null 2>&1 && rm /tmp/" + filename
     response = os.system(command)
     if response == 0:
         return True
